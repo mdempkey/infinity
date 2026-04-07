a831a79fd6ea3ed414cb305705ea68bb71defdee
BrianCassriel <bccassriel@gmail.com>
2026-04-06 19:26:22 -0700

    Fix the build

--- src/Infinity.WebApi/Models/AttractionCategory.cs
+++ src/Infinity.WebApi/Models/AttractionCategory.cs
@@ -2,8 +2,8 @@ namespace Infinity.WebApi.Models;
 
 public class AttractionCategory
 {
-    public Guid AttractionId { get; set; }
-    public Guid CategoryId { get; set; }
+    public string AttractionId { get; set; }
+    public string CategoryId { get; set; }
 
     public Attraction Attraction { get; set; } = null!;
     public Category Category { get; set; } = null!;
--- src/Infinity.WebApi/Program.cs
+++ src/Infinity.WebApi/Program.cs
@@ -13,9 +13,6 @@ builder.Services.AddScoped<IStringService, StringService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDbContext<LocationsDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("LocationsConnection")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
